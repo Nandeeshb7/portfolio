790fb2089272ed7b616ef075ad218aeaef25dc5e
Nandeeshb7 <122885314+Nandeeshb7@users.noreply.github.com>
2025-04-14 18:46:01 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import logo from '../assets/Mylogo.png';
+import { FaLinkedin, FaGithub,  FaInstagram } from 'react-icons/fa';
+function Navbar() {
+  return (
+    <nav className='mb-20 flex items-center justify-between py-6'>
+      <div className='flex flex-shrink-0 items-center'>
+        <img className='mx-2 w-24 md:w-40 h-auto' src={logo} alt='logo' />
+      </div>
+      <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+      <a href="https://www.linkedin.com/in/nandeesh-b-773a4720a/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin className='w-6 h-6 md:w-8 md:h-8' />
+        </a>
+        <a href="https://github.com/nandeeshb7" target="_blank" rel="noopener noreferrer">
+          <FaGithub className='w-6 h-6 md:w-8 md:h-8' />
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/constants/index.js
@@ -0,0 +1,98 @@
+import maisonava from "../assets/projects/maisonava.png";
+import dilipsindia from "../assets/projects/dilipsindia.png"
+import kanha from "../assets/projects/kanha.png"
+import healthportal from "../assets/projects/healthportal.png"
+
+export const HERO_CONTENT = `I am a passionate FrontEnd developer with a talent for crafting responsive and maintainable web applications.With 2+ years of hands-on experience in modern JavaScript frameworks like React, Next, Redux, and a strong background in integrating RESTful APIs, I aim to leverage my skills to create innovative and user-centric solutions that enhance productivity and user engagement.`;
+export const ABOUT_TEXT = `I am a  Junior Software Engineer with a passion for crafting seamless, user-focused web applications that drive both business growth and customer satisfaction. With a solid foundation in React.js, Next.js, and state management tools like ContextAPI and Redux Toolkit, I specialize in building responsive and scalable solutions that deliver exceptional performance across devices.
+Currently at Sparsh Communications, I focus on optimizing front-end performance using cutting-edge techniques like lazy loading, dynamic imports, and prefetching. I take pride in creating dynamic, accessible interfaces using React.js, Nextjs, Tailwind CSS.
+
+In addition to professional roles, I have spearheaded innovative projects like a Traveller, Moboom, and an E-Commerce Platform, where I implemented advanced features such as search, filtering, and real-time updates, enhancing user engagement and functionality.
+My technical skill set spans JavaScript (ES6+), HTML5, CSS3, Node.js, Redux and modern frameworks like Tailwind CSS and Bootstrap. I’m also experienced with performance optimization, dynamic workflows, and Agile collaboration.
+What excites me most is the opportunity to combine my technical expertise with creative problem-solving to deliver impactful solutions that meet both user needs and business objectives. When I’m not coding, I’m exploring new technologies, learning about design systems, or diving into a good tech blog.`;
+
+export const EXPERIENCES = [
+  {
+    year: "June 2022 - April 2024",
+    role: "Junior Software Engineer",
+    company: "Fervid Smart Solutions Pvt Ltd",
+    description: `The project aims to enhance communication and collaboration between teachers and students using modern web technologies like React.js, React Native, HTML, CSS, and JavaScript. It ensures responsive and accessible interfaces across all devices.
+Agile methodology will guide development, with tasks broken into sprints and adapted based on user feedback. Technical issues will be addressed quickly to maintain platform stability.
+Efficient task management and timely delivery are key priorities, ensuring high-quality updates. Ultimately, the goal is to build a reliable, user-friendly platform that supports a collaborative learning experience.`,
+    technologies: ["HTML", "CSS", "Javascript", "Nextjs", "Reactjs", "React Native", "Tailwind", "Ant Design"],
+  },
+  {
+    year: "Dec 2022 - January 2023",
+    role: "DashCraft: Analytics Dashboard Builder",
+    company: "Fervid Smart Solutins PVT lTD",
+    description: ` 
+    •The project aims to create a dashboard showcasing pie charts and bar charts, integrating
+code for specific charts via HTTP requests such as GET, POST, UPDATE, and DELETE
+methods.
+•This will be achieved using React libraries like Ant Design and DevExtreme.
+    `,
+    technologies: ["HTML", "CSS", "Javascript", "Reactjs", "DevExtreme","Ant Design"],
+  },
+  {
+    year: "June 2022 - Aug 2022",
+    role: "IoT Insight Hub: Antd Analytics",
+    company: "VISHIST Business Solutions Pvt Ltd",
+    description: ` Developed Next.JS/React.JS dashboards for IoT R&D performance tracking and educational
+management system using Antd.
+•Engineered an in-house IoT admin dashboard from scratch using Ant Design, visualizing
+device performance records.
+•Built the project using Next JS and Antd library, while also evaluating competitor websites for
+content, look, and feel.
+•Effectively translated client requirements into application designs and system requirements `,
+    technologies: ["HTML", "CSS", "Javascript", "Reactjs", "DevExtreme","Ant Design"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Maison Ava",
+    image: maisonava,
+    description:
+      `Maison Ava is a luxury children's clothing brand specializing in handcrafted couture for boys and girls.
+      Founded by New York-based designer Viranica Manchu, the brand emphasizes exquisite craftsmanship,
+      utilizing the world's most luxurious fabrics and intricate embellishments to create youthful yet
+      sophisticated designs.`,
+    technologies: ["HTML", "TailwindCSS", "Nextjs", "Redux", "JavaScript"],
+  },
+  {
+    title: "Dilipsindia",
+    image: dilipsindia,
+    description:
+      `DilipsIndia is a leading manufacturer of high-quality chrome-plated (CP) bathroom fittings,
+      combining international quality standards with exquisite design. Established in 2006, the company
+      boasts over 14 years of experience in the hardware industry, delivering premium products for modern
+      bathrooms.`,
+    technologies: ["HTML", "TailwindCSS", "React", "JavaScript"],
+  },
+  {
+    title: "Kanha Proposal Portal",
+    image: kanha,
+    description:
+      `Heartfulness Kanha Proposal Portal is indeed a matrimonial site specifically designed for individuals who are
+      registered with the Heartfulness organization. It is a platform where Heartfulness practitioners can
+      connect with like-minded individuals for the purpose of marriage, based on shared spiritual values and
+      practices.`,
+    technologies: ["HTML", "CSS", "JavaScript", "Bootstrap", "React", "PHP", "Laravel"],
+  },
+  {
+    title: "Union Health Patient Portal",
+    image: healthportal,
+    description:
+      `The Union Health Patient Portal is a secure online platform that enables patients to access their personal health information and communicate with healthcare providers.
+      Through the portal, patients can view portions of their medical records, including test results, medications, and visit history. Additionally, the portal offers tools for non-urgent communication,
+      such as requesting prescription refills and scheduling appointments. `,
+    technologies: ["HTML", "CSS", "JavaScript", "Next Js", "React", "Ant Design", "Devextreme",],
+  },
+
+];
+
+export const CONTACT = {
+  address: "Banglore",
+  phoneNo: "+91 7569946932",
+  email: "nandeeshb777@gmail.com",
+};
